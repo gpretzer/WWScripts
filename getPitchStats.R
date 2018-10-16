493a118c008a2b531286c1167334c993b29a77f1
AnneSWarlaumont <anne.warlaumont@gmail.com>
2018-10-16 16:37:05 -0700

    Adding getPitchStats.R, which runs getPitchStats.Praat

---
+++ getPitchStats.R
@@ -0,0 +1,6 @@
+library(tuneR)
+setwd('/Users/awarlau/Google Drive/Work/GitRepos/WWScripts/')
+uttWav = readWave('exampleUtt.wav')
+
+praatOutput = system2('/Applications/Praat.app/Contents/MacOS/Praat',args = "--run getPitchStats.Praat '/Users/awarlau/Google Drive/Work/GitRepos/WWScripts/exampleUtt.wav'",stdout = "TRUE")
+
