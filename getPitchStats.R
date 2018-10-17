97d68ca9269639419606654584d79cd00c0ec4e0
AnneSWarlaumont <anne.warlaumont@gmail.com>
2018-10-17 11:05:47 -0700

    Adding notes on where input files can be found.

--- getPitchStats.R
+++ getPitchStats.R
@@ -4,3 +4,5 @@ uttWav = readWave('exampleUtt.wav')
 
 praatOutput = system2('/Applications/Praat.app/Contents/MacOS/Praat',args = "--run getPitchStats.Praat '/Users/awarlau/Google Drive/Work/GitRepos/WWScripts/exampleUtt.wav'",stdout = TRUE)
 
+# Audio files are in WWwavFiles in our Gina Box folder
+# ELAN export files are in WWtxtFiles in Gina Box folder
